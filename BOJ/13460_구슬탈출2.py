49b07ad767514793bd469b9fd558ac38b3cded56
AhnDogeon <qltiqlti@gmail.com>
2019-06-23 23:55:36 +0900

    ss

---
+++ BOJ/13460_구슬탈출2.py
@@ -0,0 +1,44 @@
+import sys
+sys.stdin = open('13460_구슬탈출2.txt', 'r')
+
+N, M = map(int, input().split())
+
+board = []
+
+for _ in range(N):
+    board.append(list(input()))
+
+
+
+print('============디버깅============')
+for i in range(N):
+    for j in range(M):
+        print(board[i][j], end=' ')
+    print()
+print('============디버깅============')
+
+diff = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+# 북 : 0
+# 동 : 1
+# 남 : 2
+# 서 : 3
+
+B_visit = [[False] * M for _ in range(N)]
+R_visit = [[False] * M for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 'R':
+            R_index = [i, j]
+        elif board[i][j] == 'B':
+            B_index = [i, j]
+        elif board[i][j] == 'O':
+            hole = [i, j]
+        else:
+            R_visit[i][j] = True
+            B_visit[i][j] = True
+
+while True:
+    for direc in diff:
+        
