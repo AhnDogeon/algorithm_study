c3fc1516baa739766603b3f7d1c797e4a55e2afe
AhnDogeon <qltiqlti@gmail.com>
2019-08-13 19:53:52 +0900

    배열

--- BOJ/17406_배열 돌리기4.py
+++ BOJ/17406_배열 돌리기4.py
@@ -16,15 +16,46 @@ for _ in range(K):
 
 print(k_list)
 
+def dol(start, end):
+    global board
+    if start == end:
+        return
+    else:
+        tmp2 = -1
+        x = start[0]
+        y = start[1]
+        while True:
+            if y != end[1]:
+                tmp = board[x][y + 1]
+                y += 1
+                board[x][y] = tmp
+            else:
+                break
+        while True:
+            if x != end[0]:
+                tmp = board[x][y]
+                y += 1
+                board[x][y] = tmp
+            else:
+
+
+
+        start = [start[0] + 1, start[1] + 1]
+        end = [end[0] - 1, end[1] - 1]
+        dol(start, end)
+
+
+
+
 for yeon in k_list:
     minus_low = yeon[0] - yeon[2]
     minus_high = yeon[1] - yeon[2]
     plus_low = yeon[0] + yeon[2]
     plus_high = yeon[1] + yeon[2]
 
-    # (minus_low, minus_high)
-    # (plus_low, plus_high)
+    start = [minus_low, minus_high]
+    end = [plus_low, plus_high]
 
-    gap_x = plus_low - minus_low
-    gap_y = plus_high - minus_high
+    dol(start, end)
+
 
